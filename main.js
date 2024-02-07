209e6c7ef1c42df3bb5889bbc6bd7c9f34cf60e8
Haya Zubair <62325910+hayamione@users.noreply.github.com>
2024-02-07 12:20:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+import { CallClient } from "@azure/communication-calling";
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+
+let call;
+let incomingCall;
+let callAgent;
+let deviceManager;
+let tokenCredential;
+const userToken = document.getElementById("token-input");
+const calleeInput = document.getElementById("callee-id-input");
+const submitToken = document.getElementById("token-submit");
+const callButton = document.getElementById("call-button");
+const hangUpButton = document.getElementById("hang-up-button");
+const acceptCallButton = document.getElementById('accept-call-button');
+const status = document.getElementById("status");
+
+submitToken.addEventListener("click", async () => {
+    const callClient = new CallClient();
+    const userTokenCredential = userToken.value;
+    try {
+        tokenCredential = new AzureCommunicationTokenCredential(userTokenCredential);
+        callAgent = await callClient.createCallAgent(tokenCredential);
+        deviceManager = await callClient.getDeviceManager();
+        await deviceManager.askDevicePermission({ audio: true });
+        callButton.disabled = false;
+        submitToken.disabled = true;
+        // Listen for an incoming call to accept.
+        callAgent.on('incomingCall', async (args) => {
+            try {
+                incomingCall = args.incomingCall;
+                acceptCallButton.disabled = false;
+                callButton.disabled = true;
+                status.innerHTML = "Incoming Call";
+            } catch (error) {
+                console.error(error);
+            }
+        });
+        console.log("Token Connected");
+        status.innerHTML = "Token Valid";
+    } catch (error) {
+        window.alert("Please submit a valid token!");
+        status.innerHTML = "Token Invalid";
+    }
+})
+
+callButton.addEventListener("click", () => {
+    try {// start a call
+        const userToCall = calleeInput.value;
+        call = callAgent.startCall(
+            [{ id: userToCall }],
+            {},
+            console.log("Call State"),
+            console.log(call.state),
+            console.log("Call Started"),
+            status.innerHTML = "Call Started",
+            console.log("Call State"),
+            console.log(call.state),
+        );
+        // toggle button states
+
+        console.log("Call State");
+        console.log(call.state);
+        hangUpButton.disabled = false;
+        callButton.disabled = true;
+    }
+    catch (error) {
+        console.log("Start call failed");
+        console.error(error);
+        status.innerHTML = "Start call failed";
+    }
+});
+
+hangUpButton.addEventListener("click", () => {
+    // Check if the call object exists and is in a valid state for hangup
+    if (call && call.state === 'Connected') {
+        // end the current call
+        call.hangUp({ forEveryone: true });
+
+        // Update status to indicate call hangup
+        status.innerHTML = "Call ended";
+
+        // toggle button states
+        hangUpButton.disabled = true;
+        callButton.disabled = false;
+        submitToken.disabled = false;
+        acceptCallButton.disabled = true;
+    } else {
+        console.log("Call is not in a valid state for hangup.");
+        status.innerHTML = "Call is not in a valid state for hangup.";
+    }
+});
+
+
+acceptCallButton.onclick = async () => {
+    try {
+        call = await incomingCall.accept();
+        acceptCallButton.disabled = true;
+        hangUpButton.disabled = false;
+        console.log("Accept Call Button clicked");
+        status.innerHTML = "Call accept successfull";
+    } catch (error) {
+        console.log("Accept call failed");
+        console.error(error);
+        status.innerHTML = "Accept call failed";
+    }
+}
